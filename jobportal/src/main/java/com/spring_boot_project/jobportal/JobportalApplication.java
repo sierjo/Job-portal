de3435be821130af7d1729846839fcde57d39551
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-12-13 16:14:04 +0300

    Nothing has change

---
+++ jobportal/src/main/java/com/spring_boot_project/jobportal/JobportalApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_project.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportalApplication.class, args);
+	}
+
+}
---
+++ jobportal/src/test/java/com/spring_boot_project/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_project.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
